92631b27feb4162527328939e7475f31beccffa4
Aidana Niiazova <aidana.niiazova@gmail.com>
2020-02-22 14:49:04 +0600

    pages about, contacts

--- src/components/pages/contacts/contacts.js
+++ src/components/pages/contacts/contacts.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import './contacts.css';
 
-const contacts = "Здесь будут контакты";
 const Contacts = () => {
     return (
-        <div className="container">
-            {contacts}
+        <div className="container user-form">
+            <Form>
+            <FormGroup>
+                <Label for="examplePassword">Имя
+                    <span className="required">*</span>
+                </Label>
+                <Input type="password" name="name" id="exampleName" placeholder="Имя" />
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="exampleEmail">Email
+                    <span className="required">*</span>
+                </Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="email" />
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="exampleText">Ваше сообщение</Label>
+                <Input type="textarea" name="text" id="exampleText" />
+            </FormGroup>
+
+            <Button>Submit</Button>
+        </Form>
         </div>
     )
 }

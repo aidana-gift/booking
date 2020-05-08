021cbd605c91bf43a8df189b88042ec8868b655a
Aidana Niiazova <aidana.niiazova@gmail.com>
2020-05-08 16:56:54 +0600

    router fix

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import i18n from './i18n/i18n';
 import { I18nextProvider } from "react-i18next";
+import { BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
   <Suspense fallback={(<div>Loading...</div>)}>
-    <App/>
+    <Router>
+      <App/>
+    </Router>
   </Suspense>
   </I18nextProvider>,
   document.getElementById('root'));
